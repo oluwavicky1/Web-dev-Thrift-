4f03576455de7740f30ebdaefb7ca8710c8794d3
Iwatannaye Victor <iwatannayevictor@gmail.com>
2021-05-04 18:11:25 +0100

    updated register function

--- front-end/script.js
+++ front-end/script.js
@@ -1,12 +1,38 @@
-function Register() {
-  // e.preventDefault();
+let xhr = new XMLHttpRequest();
+// xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+const baseUrl = "http://192.168.8.102/Web Project/api/";
+
+async function Register() {
+  let type;
+  var ele = document.getElementsByName("type");
+
+  for (i = 0; i < ele.length; i++) {
+    if (ele[i].checked) {
+      type = ele[i].value;
+    }
+  }
   const data = JSON.stringify({
-    firstname: document.getElementById("Firstname").innerHTML,
-    surname: document.getElementById("Lastname").innerHTML,
-    type: document.getElementById("Type").innerHTML,
-    email: document.getElementById("Email").innerHTML,
-    password: document.getElementById("Password").innerHTML,
+    firstName: document.getElementById("Firstname").value,
+    surname: document.getElementById("Lastname").value,
+    type: type,
+    email: document.getElementById("Email").value,
+    password: document.getElementById("Password").value,
   });
 
   console.log(data);
+
+  xhr.open("POST", `http://192.168.8.102/Web Project/api/user/register.php`);
+  xhr.send(data);
+
+  xhr.onload = function () {
+    // if (xhr.status != 200) {
+    // HTTP error?
+    // handle error
+    //   alert( 'Error: ' + xhr.status);
+    console.log(xhr.response);
+    return;
+    // }
+
+    // get the response from xhr.response
+  };
 }
