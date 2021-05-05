1a269c0bb5ba6ab20de08fa272fa7fa4b64d5bd0
Iwatannaye Victor <iwatannayevictor@gmail.com>
2021-05-05 23:19:01 +0100

    made updates on the sign in and js

--- front-end/script.js
+++ front-end/script.js
@@ -1,6 +1,6 @@
 let xhr = new XMLHttpRequest();
 // xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
-const baseUrl = "http://192.168.8.102/Web Project/api/";
+const baseUrl = "http://localhost:80/Web Project/api/";
 
 //Function to register a user
 async function Register() {
@@ -20,7 +20,7 @@ async function Register() {
     password: document.getElementById("Password").value,
   });
 
-  xhr.open("POST", `http://192.168.8.102/Web Project/api/user/register.php`);
+  xhr.open("POST", `http://localhost:80/Web Project/api/user/register.php`);
   xhr.send(data);
 
   xhr.onload = function () {
@@ -44,21 +44,42 @@ async function SignIn() {
     password: document.getElementById("Password").value,
   });
 
-  xhr.open("POST", `http://192.168.8.102/Web Project/api/user/login.php`);
+  xhr.open("POST", `http://localhost:80/Web Project/api/user/login.php`);
   xhr.send(data);
 
   xhr.onload = function () {
     let resp = JSON.parse(xhr.response);
     if (xhr.status == 200) {
       alert(resp.message);
-      //   window.location.replace("http://127.0.0.1:5500/front-end/meetings.html");
+
       //   save user's details in a session
       sessionStorage.setItem("user", resp.data);
+      if (resp.data.type == "STUDENT") {
+        window.location.replace(
+          "http://127.0.0.1:5500/front-end/Student-Meetings.html"
+        );
+      } else {
+        window.location.replace(
+          "http://127.0.0.1:5500/front-end/supervisor-Meetings.html"
+        );
+      }
     } else {
       // handle error
       // get the response from xhr.response
 
       alert("Error: " + resp.message);
     }
+  };
+}
+
+//get seemesters
+async function get_seemesters() {
+  xhr.open("GET", `http://localhost:80/Web Project/api/semester/semester.php`);
+  xhr.send(data);
+
+  xhr.onload = function () {
+    let resp = JSON.parse(xhr.response);
+
+    return resp.data;
   };
 }
